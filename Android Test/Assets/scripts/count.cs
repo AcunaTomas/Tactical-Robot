0aad0fd07e631fad53351f8dbed3405e83dc0f83
MartinianoMarelli <54485319+MartinianoMarelli@users.noreply.github.com>
2022-04-20 18:07:54 -0300

    Animator Update
    Animator Update

--- Android Test/Assets/scripts/count.cs
+++ Android Test/Assets/scripts/count.cs
@@ -7,22 +7,33 @@ public class count : MonoBehaviour
 {
     private NavMeshAgent an;
     public Touch asw;
+    public Animator animator;
+    public Transform trs;
+
     // Start is called before the first frame update
     void Start()
     {
        an = GetComponent<NavMeshAgent>();
-       
+       animator = GetComponent<Animator>();
+	 trs = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+	   if (trs.position == an.destination)
+	   {
+
+		animator.SetBool("move", false);
+
+	   }
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) 
             {
                 an.destination = hit.point;
+			animator.SetBool("move", true);
             }
 
             if (Input.touchCount > 0)
@@ -32,9 +43,10 @@ public class count : MonoBehaviour
                 if(Physics.Raycast(Camera.main.ScreenPointToRay(asw.position), out hit)) 
                 {
                     an.destination = hit.point;
+			    animator.SetBool("move", true);
                 }
             }
-
+		
 
         }
     }
